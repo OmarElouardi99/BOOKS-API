63a3e19c58b9d6d55cad5f15f13a126fc85f5642
OmarElOUARDI <omar@bifolia.ma>
2022-09-03 23:34:45 +0100

    DEV - add token model

--- internal/data/models.go
+++ internal/data/models.go
@@ -10,4 +10,16 @@ type user struct {
 	Password  string    `json:"password"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
+	Token     token     `json:"token"`
+}
+
+type token struct {
+	Id        int       `json:"id"`
+	UserId    int       `json:"user_id"`
+	Email     string    `json:"email"`
+	Token     string    `json:"token"`
+	TokenHash []byte    `json:"_"`
+	Expiry    time.Time `json:"expiry"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
